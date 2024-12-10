17b2f8d17ac0836e5b95ad08cd77f14e347e9b75
evanderjar <evanderjar@gmail.com>
2024-12-10 16:21:35 -0400

    se agrego mapa

--- src/screens/map/index.js
+++ src/screens/map/index.js
@@ -1,18 +1,97 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, PermissionsAndroid, Platform } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 import { commonStyles } from '../../style/commonStyle.css';
 
 const MapComponent = ({ initialRegion }) => {
-  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [location, setLocation] = useState(null);
 
   const mapStyle = [
     // Aquí puedes añadir tu estilo personalizado para el mapa
   ];
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permiso de ubicación',
+          message: 'Esta aplicación necesita acceso a tu ubicación',
+          buttonNeutral: 'Pregúntame más tarde',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'OK',
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Permiso de ubicación concedido');
+        // getCurrentLocation();
+        watchLocation()
+      } else {
+        console.log('Permiso de ubicación denegado');
+      }
+    } else {
+      watchLocation()
+      // getCurrentLocation();
+    }
+  };
 
-  const handleMapPress = (event) => {
-    const { latitude, longitude } = event.nativeEvent.coordinate;
-    setSelectedLocation({ latitude, longitude });
+  useEffect(() => {
+
+    requestLocationPermission();
+
+  }, []);
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        });
+      },
+      (error) => {
+        console.log('Error al obtener la ubicación:', error.message, error.code);
+        if (error.code === 3) { // Timeout
+          // Prueba a usar watchPosition como alternativa
+          watchLocation();
+        }
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 30000, // Incrementar el tiempo de espera a 30 segundos
+        maximumAge: 0, // Asegurarse de obtener una ubicación nueva
+      }
+    );
+  };
+
+  const watchLocation = () => {
+
+    console.log("Aquiiiiiii")
+    const watchId = Geolocation.watchPosition(
+      (position) => {
+        console.log(position)
+        const { latitude, longitude } = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        });
+        Geolocation.clearWatch(watchId); // Detener watchPosition después de obtener la ubicación
+      },
+      (error) => {
+        console.log('Error al observar la ubicación:', error.message, error.code);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 30000,
+        maximumAge: 0,
+        distanceFilter: 1, // Distancia mínima (en metros) que debe cambiar antes de actualizar la ubicación
+      }
+    );
   };
 
   return (
@@ -21,16 +100,16 @@ const MapComponent = ({ initialRegion }) => {
         ¿Ya se encuentra registrado?
       </Text>
       <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={initialRegion}
+        region={location || initialRegion}
         customMapStyle={mapStyle}
-        onPress={handleMapPress}
+        showsUserLocation={true}
       >
-        {selectedLocation && (
+        {location && (
           <Marker
-            coordinate={selectedLocation}
-            title="Ubicación Seleccionada"
+            coordinate={location}
+            title="Tu ubicación actual"
           />
         )}
       </MapView>
@@ -45,7 +124,7 @@ const styles = StyleSheet.create({
     width: 300,
   },
   map: {
-    ...StyleSheet.absoluteFillObject, // Esto asegura que el mapa ocupe todo el contenedor
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
