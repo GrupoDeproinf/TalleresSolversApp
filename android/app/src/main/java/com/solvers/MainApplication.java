f1726346a436bfd70046ee35512b1d47e1621d5d
gabrieladeproinf <gabriela.deproinf@gmail.com>
2025-01-31 10:18:16 -0400

    Cambios

--- android/app/src/main/java/com/solvers/MainApplication.java
+++ android/app/src/main/java/com/solvers/MainApplication.java
@@ -1,5 +1,5 @@
-package com.pixelstrap.mido;
-
+package com.solvers;
+import com.solvers.app.BuildConfig;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -56,10 +56,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
-    }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
