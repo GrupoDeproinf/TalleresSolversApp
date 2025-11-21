1b4785c5744abccb7aca1ebe44acf1724793d64c
César Lozada <cesar.lozada.deproinf@gmail.com>
2025-11-21 12:56:52 -0400

    FULL VERSION

--- build.ps1
+++ build.ps1
@@ -45,7 +45,7 @@ Push-Location android
 Write-Host "-----------------------------------------"
 # Write-Host "GENERANDO APK..."
 # ./gradlew assembleRelease
-Write-Host "-----------------------------------------"
+# Write-Host "-----------------------------------------"
 Write-Host "GENERANDO BUNDLE"
 ./gradlew bundleRelease
 Pop-Location
